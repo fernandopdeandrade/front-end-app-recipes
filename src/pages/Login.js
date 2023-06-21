a8b6ea3f7b0060e336f6614669b3438b1e335845
fernandopdeandrade <pupygreen@gmail.com>
2023-06-20 21:33:17 -0300

    feat: mensagns de sucesso e de erro ao cadastrar,  efetuar, ou tentar efetuar login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,26 +1,27 @@
-import { useHistory, Link } from 'react-router-dom';
-import { useState, useEffect, useContext } from 'react';
-import recipesLogo from '../images/recipesLogo.png';
-import restaurante from '../images/restaurante.png';
-import useDataInfos from '../hooks/useDataInfos';
-import Loading from '../components/Loading';
-import { FilterContextState } from '../context/InfoContext';
-import '../styles/Login.css';
+import { useContext, useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import Loading from "../components/Loading";
+import { FilterContextState } from "../context/InfoContext";
+import useDataInfos from "../hooks/useDataInfos";
+import recipesLogo from "../images/recipesLogo.png";
+import restaurante from "../images/restaurante.png";
+import "../styles/Login.css";
 
 function Login() {
   const history = useHistory();
 
   const { role } = useContext(FilterContextState) || {};
 
-  const { loginUser, errorLogin, setErrorLogin, isLoading } = useDataInfos() || {};
+  const { loginUser, errorLogin, setErrorLogin, isLoading } =
+    useDataInfos() || {};
 
-  const [formEmail, setFormEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [messageLogin, setMessageLogin] = useState('');
+  const [formEmail, setFormEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [messageLogin, setMessageLogin] = useState("");
 
   const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
   const emailValidation = formEmail.match(emailRegex);
-  const PASSWORD_LIMIT = 6;
+  const PASSWORD_LIMIT = 5;
   const passwordValidation = password.length > PASSWORD_LIMIT;
 
   const saveSubmition = async () => {
@@ -28,29 +29,29 @@ function Login() {
   };
 
   useEffect(() => {
-    if (errorLogin === 'Invalid email or password') {
-      setErrorLogin('Email ou senha inválidos');
+    if (errorLogin === "Invalid email or password") {
+      setErrorLogin("Email ou senha inválidos");
 
       setTimeout(() => {
-        setErrorLogin('');
-      }, 3000)
+        setErrorLogin("");
+      }, 3000);
     }
 
-    if (errorLogin === 'Invalid email or password 1') {
-      setErrorLogin('Usuário não encontrado no banco de dados');
+    if (errorLogin === "Invalid email or password 1") {
+      setErrorLogin("Usuário não encontrado no banco de dados");
 
       setTimeout(() => {
-        setErrorLogin('');
-      }, 3000)
+        setErrorLogin("");
+      }, 3000);
     }
 
-    if (role === 'admin' || role === 'user') {
-      history.push('/meals');
-    } if (role === '') {
-      setMessageLogin('Preencha todos os campos!');
+    if (role === "admin" || role === "user") {
+      history.push("/meals");
+    }
+    if (role === "") {
+      setMessageLogin("Preencha todos os campos!");
     }
   }, [role, history, errorLogin, setErrorLogin, setMessageLogin]);
-
 
   if (isLoading) return <Loading />;
 
@@ -81,16 +82,15 @@ function Login() {
         >
           Entrar
         </button>
-        <Link
-          className="link"
-          to="/register"
-        >
+        <Link className="link" to="/register">
           Ainda não tem conta?
         </Link>
       </div>
-      {errorLogin.length > 0
-        ? <span className="span-error-login">{errorLogin}</span>
-        : <span className="span-message">{messageLogin}</span>}
+      {errorLogin.length > 0 ? (
+        <span className="span-error-login">{errorLogin}</span>
+      ) : (
+        <span className="span-message">{messageLogin}</span>
+      )}
     </div>
   );
 }
